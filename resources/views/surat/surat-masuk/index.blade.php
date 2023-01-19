d0fa6d7f65cf63a2cc23cc7e9a421bbf37c0bd89
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-19 17:27:09 +0700

    feat: add conditioan tambah surat class

--- resources/views/surat/surat-masuk/index.blade.php
+++ resources/views/surat/surat-masuk/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="p-5 rounded-lg bg-white">
-        <div class="flex justify-between items-center pb-4">
+        <div class="flex @can('tata usaha') justify-between @else justify-end @endcan items-center pb-4">
             @can('tata usaha')
                 <a href="{{ route('surat.masuk.create') }}"
                     class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
@@ -158,6 +158,7 @@
                                 </td>
                             @endcanany
                             <td class="py-4 px-6">
+                                Disposisi
                             </td>
                         </tr>
                     @endforeach
