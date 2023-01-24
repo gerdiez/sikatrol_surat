45467004b67485a31c921485ee62103570f20f8e
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-24 16:06:47 +0700

    feat: add 12 user for unit kerja

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,17 +22,94 @@ class UserSeeder extends Seeder
         ])->assignRole('tata');
 
         User::create([
-            'name' => 'Unit Kerja',
-            'username' => 'unitkerja',
-            'email' => 'unitkerja@regol.go.id',
-            'password' => bcrypt('Admin@123'),
-        ])->assignRole('unit');
-
-        User::create([
             'name' => 'Sekretaris Kecamatan',
             'username' => 'sekretaris',
             'email' => 'sekretaris@regol.go.id',
             'password' => bcrypt('Admin@123'),
         ])->assignRole('sekre');
+
+        User::create([
+            'name' => 'Umpeg',
+            'username' => 'umpeg',
+            'email' => 'umpeg@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Kesejahteraan Sosial',
+            'username' => 'kesos',
+            'email' => 'kesos@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Pemberdayaan Masyarakat',
+            'username' => 'pm',
+            'email' => 'pm@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Pemerintahan',
+            'username' => 'pemerintahan',
+            'email' => 'pemerintahan@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Tramtib',
+            'username' => 'tramtib',
+            'email' => 'tramtib@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Kelurahan Ancol',
+            'username' => 'ancol',
+            'email' => 'ancol@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Kelurahan Balonggede',
+            'username' => 'balonggede',
+            'email' => 'balonggede@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Kelurahan Ciseureuh',
+            'username' => 'ciseureuh',
+            'email' => 'ciseureuh@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Kelurahan Cigereleng',
+            'username' => 'cigereleng',
+            'email' => 'cigereleng@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Kelurahan Ciateul',
+            'username' => 'ciateul',
+            'email' => 'ciateul@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Kelurahan Pungkur',
+            'username' => 'pungkur',
+            'email' => 'pungkur@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
+
+        User::create([
+            'name' => 'Kelurahan Pasirluyu',
+            'username' => 'pasirluyu',
+            'email' => 'pasirluyu@regol.go.id',
+            'password' => bcrypt('Admin@123'),
+        ])->assignRole('unit');
     }
 }
