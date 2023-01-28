ce93cf374fcc2c225e1f331fb678287b41065e5f
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-28 13:11:34 +0700

    feat: refactoring create surat masuk belum disposisi

--- app/Http/Controllers/SuratMasuk/MasukBelumController.php
+++ app/Http/Controllers/SuratMasuk/MasukBelumController.php
@@ -25,48 +25,31 @@ class MasukBelumController extends Controller
 
     public function store(Request $request)
     {
-        if ($request->input('disposisi') == null) {
-            $validate = $request->validate([
-                'surat_dari' => 'required',
-                'jenis_surat' => 'required',
-                'no_surat' => 'required',
-                'tanggal_surat' => 'required',
-                'sifat' => 'required',
-                'no_agenda' => 'required',
-                'tanggal_kegiatan' => 'required',
-                'kategori' => 'required',
-                'perihal' => 'required',
-                'file' => 'required|mimes:csv,txt,xlx,xls,pdf|max:2048',
-                'disposisi' => ''
-            ]);
-            $fileName = $request->file('file')->getClientOriginalName();
-            $validate['file_name'] = $fileName;
-            $validate['file'] = $request->file('file')->storeAs('files', $fileName);
-            Surat::create($validate);
-            return redirect('/surat-masuk/belum-disposisi');
-        } else {
-            $validate = $request->validate([
-                'surat_dari' => 'required',
-                'jenis_surat' => 'required',
-                'no_surat' => 'required',
-                'tanggal_surat' => 'required',
-                'sifat' => 'required',
-                'no_agenda' => 'required',
-                'tanggal_kegiatan' => 'required',
-                'kategori' => 'required',
-                'perihal' => 'required',
-                'file' => 'required|mimes:csv,txt,xlx,xls,pdf|max:2048',
-                'disposisi' => '',
-                'diteruskan_ke' => '',
-                'catatan' => '',
-                'dari' => '',
-            ]);
-            $fileName = $request->file('file')->getClientOriginalName();
-            $validate['file_name'] = $fileName;
-            $validate['file'] = $request->file('file')->storeAs('files', $fileName);
-            Surat::create($validate);
-            return redirect('/surat-masuk/sudah-disposisi');
+        $validate = $request->validate([
+            'surat_dari' => 'required',
+            'jenis_surat' => 'required',
+            'no_surat' => 'required',
+            'tanggal_surat' => 'required',
+            'sifat' => 'required',
+            'no_agenda' => 'required',
+            'tanggal_kegiatan' => 'required',
+            'kategori' => 'required',
+            'perihal' => 'required',
+            'file' => 'required|mimes:csv,txt,xlx,xls,pdf|max:2048',
+            'disposisi' => ''
+        ]);
+
+        if ($request->input('disposisi') == true) {
+            $validate['diteruskan_ke'] = '';
+            $validate['catatan'] = '';
+            $validate['dari'] = '';
         }
+
+        $fileName = $request->file('file')->getClientOriginalName();
+        $validate['file_name'] = $fileName;
+        $validate['file'] = $request->file('file')->storeAs('files', $fileName);
+        Surat::create($validate);
+        return redirect('/surat-masuk/sudah-disposisi');
     }
 
     public function show($id)
