e09699a800f8a94d4cd9e026d00f97edf6fac66f
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-29 15:27:38 +0700

    feat: done index pengajuan & disetujui

--- app/Http/Livewire/SuratKeluar/Disetujui.php
+++ app/Http/Livewire/SuratKeluar/Disetujui.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Livewire\SuratKeluar;
 
+use App\Models\Surat;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Disetujui extends Component
 {
+    use WithPagination;
+
+    public $search;
+    public $paginate = 10;
+    public $category;
+
     public function render()
     {
-        return view('livewire.surat-keluar.disetujui');
+        $surat = Surat::latest();
+        $this->search === null
+            ? $surat
+                ->where("status", "Disetujui")
+                ->where("jenis_surat", "Surat Keluar")
+            : $surat
+                ->where("status", "Disetujui")
+                ->where("jenis_surat", "Surat Keluar")
+                ->where($this->category, "like", "%" . $this->search . "%");
+
+        return view("livewire.surat-keluar.disetujui", [
+            "surats" => $surat->paginate($this->paginate),
+            "categories" => [
+                "no_surat" => "No Surat",
+                "surat_dari" => "Surat Dari",
+                "kategori" => "Kategori",
+                "perihal" => "Perihal",
+                "sifat" => "Sifat",
+                "tanggal_kegiatan" => "Tgl Kegiatan",
+            ],
+            "options" => [10, 20, 30],
+        ]);
     }
 }
