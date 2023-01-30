151c0645c23d80bcaa6aa3fba97990f47740fbc5
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-30 13:11:14 +0700

    deploy: update main.yml to basic deploy

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -4,8 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title }} - SIKATROL</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    {{-- @vite('resources/css/app.css')
+    @vite('resources/js/app.js') --}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-dd2a727b.css') }}">
+    <script src="{{ asset('build/assets/app-76774767.js') }}"></script>
 </head>
 
 <body>
