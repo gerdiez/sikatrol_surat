ce47a07842215ffaadeed966289586be29d18da1
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-30 13:43:18 +0700

    fix: resouce layout main & delete password for prod

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
                         </div>
 
                         <div class="mb-6">
-                            <input type="password" name="password" value="Admin@123"
+                            <input type="password" name="password"
                                 class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 id="password" placeholder="Password" />
                         </div>
