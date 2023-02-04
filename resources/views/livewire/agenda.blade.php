614932be42834ee6eeaadd8c37a6872aeb5ace81
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-02-04 23:24:39 +0700

    feat: try change to yellow

--- resources/views/livewire/agenda.blade.php
+++ resources/views/livewire/agenda.blade.php
@@ -1,7 +1,7 @@
 <div class="p-5 rounded-lg bg-white">
     <div class="flex justify-between items-center">
         <a href="{{ route('agenda.show', 'export') }}"
-            class="text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            class="text-white bg-gradient-to-r from-yellow-500 via-yellow-600 to-yellow-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
             Download Excel</a>
         <div class="flex gap-5">
             <div class="relative flex">
