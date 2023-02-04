7332adfef6ef811f1ea6dcbf0d6757588ce782ef
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-02-04 23:18:10 +0700

    fix: bring back to blue

--- resources/views/livewire/agenda.blade.php
+++ resources/views/livewire/agenda.blade.php
@@ -1,7 +1,7 @@
 <div class="p-5 rounded-lg bg-white">
     <div class="flex justify-between items-center">
         <a href="{{ route('agenda.show', 'export') }}"
-            class="text-white bg-gradient-to-r bg-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
             Download Excel</a>
         <div class="flex gap-5">
             <div class="relative flex">
