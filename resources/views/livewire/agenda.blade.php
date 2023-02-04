6ca263dd75e68086b4abf9912edd6112bf5dfffe
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-02-04 23:29:54 +0700

    fix: agenda green with style with fix

--- resources/views/livewire/agenda.blade.php
+++ resources/views/livewire/agenda.blade.php
@@ -1,7 +1,7 @@
 <div class="p-5 rounded-lg bg-white">
     <div class="flex justify-between items-center">
         <a href="{{ route('agenda.show', 'export') }}"
-            class="text-white bg-gradient-to-r from-yellow-500 via-yellow-600 to-yellow-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            class="text-white hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" style="background: linear-gradient(to right, #10B981, #059669, #047857);">
             Download Excel</a>
         <div class="flex gap-5">
             <div class="relative flex">
